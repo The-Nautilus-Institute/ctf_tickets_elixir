7ad2f5b2ea1a926604a6fd092200a6ffc792d747
Vito <vito@nautilus.institute>
2023-03-24 22:02:49 -0400

    initial

---
+++ lib/ctf_tickets.ex
@@ -0,0 +1,18 @@
+defmodule CtfTickets do
+  @moduledoc """
+  Documentation for `CtfTickets`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CtfTickets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ctf_tickets_test.exs
@@ -0,0 +1,8 @@
+defmodule CtfTicketsTest do
+  use ExUnit.Case
+  doctest CtfTickets
+
+  test "greets the world" do
+    assert CtfTickets.hello() == :world
+  end
+end
